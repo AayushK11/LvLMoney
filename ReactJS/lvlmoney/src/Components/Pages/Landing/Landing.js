d50088247f8f649dc23a3fb4f67a85dcf0fc0001
Ajinkya R <ajinkyarajkar@gmail.com>
2021-11-02 14:37:48 +0530

    commit2

--- ReactJS/lvlmoney/src/Components/Pages/Landing/Landing.js
+++ ReactJS/lvlmoney/src/Components/Pages/Landing/Landing.js
@@ -1,18 +1,49 @@
-import React, { Component } from 'react'
-import Footer from '../../Parts/Footer/Footer'
-import CenteredHero from '../../Parts/Heros/CenteredHero'
-import Navbar from '../../Parts/Navbar/Navbar'
-
-
+import React, { Component } from "react";
+import Footer from "../../Parts/Footer/Footer";
+import { Hero } from "../../Parts/Heros/Hero";
+import Navbar from "../../Parts/Navbar/Navbar";
+import hero1 from "../../Images/herosong.png";
+import hero2 from "../../Images/hero2.png";
+import hero3 from "../../Images/hero3.png";
+import "./Landing.css";
 
 export default class Landing extends Component {
-    render() {
-        return (
-            <>
-              <Navbar/>
-              <CenteredHero/>
-              <Footer/>  
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <Navbar />
+        <div className="custom">
+          <Hero
+          isVisible={ true}
+          heading="LvLMoney Finance With AI"
+          subheading1="Place your money where it will grow the most."
+          subheading2="Use one app to manage all of your investments, including stocks, mutual funds, and ETFs, as well as expenses."
+          button_text="Get Started"
+          button_link="www.google.com"
+          src={hero1}
+          />
+          
+          <Hero
+          isVisible={ false}
+          heading="Its Time To Earn More"
+          button_text="Login"
+          subheading1="I'm beggin', beggin' you......"
+          subheading2="To invest somewhere other than FDs"
+          button_link="www.google.com"
+          src={hero2}
+          />
+           <Hero
+          isVisible={ false}
+          heading="Let's Invest"
+          button_text="Login"
+          subheading1="But When and Where to Invest?"
+          subheading2="Use our strategies and AI Prediction tool to find it Out."
+          button_link="www.google.com"
+          src={hero3}
+          />
+          </div>
+        <Footer />
+      </>
+    );
+  }
 }
--- ReactJS/lvlmoney/src/Components/Parts/Footer/Footer.js
+++ ReactJS/lvlmoney/src/Components/Parts/Footer/Footer.js
@@ -1,41 +1,91 @@
 import React, { Component } from "react";
 import logo from "../../Images/lvl_dark.svg";
-import "./Footer.css"
+import "./Footer.css";
 
 export default class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: window.location.origin,
+    };
+    this.generateURLs = this.generateURLs.bind(this);
+  }
+
+  generateURLs() {
+    this.setState({
+  
+      support: this.state.url.concat("/faq"),
+      privacypolicy: this.state.url.concat("/privacypolicy"),
+      termsandconditions: this.state.url.concat("/termsandconditions"),
+  
+    });
+  }
+
+  componentDidMount() {
+    this.generateURLs();
+  }
+
   render() {
     return (
       <div className="text-white bg-dark footer">
         <div class="container ">
           <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-            
-
             <a
-              href="https://www.google.com/"
-              class="col-md-4 d-flex align-items-center justify-content mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
+              href={ window.location.origin.concat("/")}
+              class="col-md-2 d-flex align-items-center justify-content mb-3 mb-md-0 me-md-auto link-dark text-decoration-none"
             >
-                <img src={logo} className="footer-logo" alt="Footer logo"/>
+              <img src={logo} className="footer-logo" alt="Footer logo" />
             </a>
-           
-            <ul class="nav col-md-4 justify-content-end">
+            <ul class="nav col-md-4 justify-content-start">
               <li class="nav-item">
-                <a href="https://www.google.com/" class="nav-link px-2 text-muted">
-               Support
+                <a
+                  href={this.state.termsandconditions }
+                  class="nav-link px-2 text-muted"
+                >
+                  Terms And Conditions
                 </a>
               </li>
               <li class="nav-item">
-                <a href="https://www.google.com/" class="nav-link px-2 text-muted">
-                  Features
+                <a
+                  href={ this.state.privacypolicy}
+                  class="nav-link px-2 text-muted"
+                >
+                  Privacy Policy
+                </a>
+              </li>
+            </ul>
+
+            <ul class="nav col-md-6 justify-content-end">
+              <li class="nav-item">
+                <a
+                  href={ this.state.support}
+                  class="nav-link px-2 text-muted"
+                >
+                  Support
                 </a>
               </li>
               <li class="nav-item">
-                <a href="https://www.google.com/" class="nav-link px-2 text-muted">
-                  FAQs
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="https://www.google.com/" class="nav-link px-2 text-muted">
+                <a
+                  href="https://www.google.com/"
+                  class="nav-link px-2 text-muted"
+                >
                   About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  href="https://www.google.com/"
+                  class="nav-link px-2 text-muted"
+                >
+                  Login
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  href="https://www.google.com/"
+                  class="nav-link px-2 text-muted"
+                >
+                  Register
                 </a>
               </li>
             </ul>
