d3fd3026ce092b0020d4a9c7872ec24a681d2072
AayushK11 <aayush.kumaria@gmail.com>
2022-06-03 15:22:20 +0530

    Minor Server Edit

--- Server/LvLMoney/Extras/getstocks_list.py
+++ Server/LvLMoney/Extras/getstocks_list.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 def get_stocks():
-    data=pd.read_csv("Databases\ind_nifty50list.csv")
-    return data.values.tolist()
+    data = pd.read_csv("Databases\ind_nifty50list.csv")
+    return data["Symbol"].values.tolist()
